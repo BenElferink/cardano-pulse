1b970d5ca925629968185240d069320aff398630
Ben Elferink <ben.elferink@icloud.com>
2021-08-26 16:35:06 +0300

    re-structured repo dirs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import {useEffect, useRef, useState} from 'react'
+import {useEffect, useState} from 'react'
+import PulseCanvas from './components/PulseCanvas'
 import logo from './images/cardano-logo-1024x1024.png'
 
 const ticker = 'ADABUSD'
 const canvasColor = '#3564f6'
 const maxQueue = 100
 
-let animationFrameId
-let pulseCount = 0
-
 function App() {
   const [dataPoints, setDataPoints] = useState([])
-  const canvasRef = useRef(null)
 
   useEffect(() => {
     // get price data on an interval basis (every 1 second)
@@ -18,100 +15,22 @@ function App() {
       () =>
         fetch(`https://www.binance.com/api/v3/ticker/price?symbol=${ticker}`)
           .then(async (response) => {
-            const {symbol, price} = await response.json()
+            const {price} = await response.json()
             // add the price (and some extra data) to the state array,
             // whilst making sure the queue length doesn't exceed the allowed number (see variable "maxQueue")
             setDataPoints((prev) => {
               while (prev.length >= maxQueue) prev.shift()
-              return [
-                ...prev,
-                {symbol, price: Number(Number(price).toFixed(4)), timestamp: Date.now()},
-              ]
+              return [...prev, {price: Number(Number(price).toFixed(4)), timestamp: Date.now()}]
             })
           })
           .catch((error) => console.error(error)),
       1000,
     )
-    return () => clearInterval(interval)
-  }, [])
-
-  useEffect(() => {
-    const canvas = canvasRef.current
-
-    // set width and height according to screen size.
-    // this is also applied when the screen size changes
-    canvas.width = window.innerWidth - 100
-    canvas.height = window.innerHeight - 420
-
-    // eslint-disable-next-line
-  }, [window.innerWidth, window.innerHeight])
-
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
-
-    // set colors of canvas items.
-    // basically required only once, but it won't hurt performance by applying this again when this useEffect is called
-    ctx.strokeStyle = canvasColor
-    ctx.fillStyle = canvasColor
-
-    // the animation draw function, creates the visuals on the canvas
-    const draw = () => {
-      pulseCount++
-      let maxPrice = 0
-      let minPrice = 0
-
-      // find the highest price, and lowest price in the current queue
-      dataPoints.forEach(({price}) => {
-        if (price > maxPrice) {
-          maxPrice = price
-        } else if (price < minPrice || minPrice === 0) {
-          minPrice = price
-        }
-      })
-
-      // generic calculators to get the X and Y positions for each data point
-      const pulseRadius = 7 * Math.sin(pulseCount * 0.05) ** 2
-      const getX = (index) => (canvas.width / maxQueue) * index
-      const getY = (price) =>
-        Math.abs((canvas.height / (maxPrice - minPrice)) * (price - minPrice) - canvas.height)
-
-      // draw the pulse dot
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-      ctx.beginPath()
-      ctx.arc(
-        dataPoints.length >= canvas.width
-          ? getX(canvas.width - pulseRadius)
-          : dataPoints.length
-          ? getX(dataPoints.length - 1)
-          : getX(0),
-        dataPoints.length ? getY(dataPoints[dataPoints.length - 1].price) : getY(0),
-        pulseRadius,
-        0,
-        2 * Math.PI,
-      )
-      ctx.fill()
-
-      // draw the graph lines
-      ctx.beginPath()
-      dataPoints.forEach(({price}, i) => {
-        if (i === 0) {
-          ctx.moveTo(getX(i), getY(price))
-        } else {
-          ctx.lineTo(getX(i), getY(price))
-        }
-      })
-      ctx.stroke()
-
-      animationFrameId = window.requestAnimationFrame(draw)
-    }
-
-    draw()
 
     return () => {
-      window.cancelAnimationFrame(animationFrameId)
+      clearInterval(interval)
     }
-  }, [dataPoints])
+  }, [])
 
   return (
     <div className='app flex-col'>
@@ -121,7 +40,7 @@ function App() {
           ${dataPoints.length ? dataPoints[dataPoints.length - 1].price : 0}
         </span>
       </header>
-      <canvas ref={canvasRef} />
+      <PulseCanvas dataPoints={dataPoints} maxPoints={maxQueue} color={canvasColor} />
     </div>
   )
 }
---
+++ src/components/PulseCanvas.js
@@ -0,0 +1,89 @@
+import {useEffect, useRef} from 'react'
+
+function PulseCanvas({dataPoints, maxPoints, color}) {
+  const canvasRef = useRef(null)
+  const animationFrameId = useRef(0)
+  const pulseCount = useRef(0)
+
+  useEffect(() => {
+    // set width and height according to screen size.
+    // this is also applied when the screen size changes
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth - 100
+    canvas.height = window.innerHeight - 420
+
+    // set colors of canvas items.
+    // basically required only once, but it won't hurt performance by applying this again when this useEffect is called
+    const context = canvas.getContext('2d')
+    context.strokeStyle = color
+    context.fillStyle = color
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [window.innerWidth, window.innerHeight, color])
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+
+    // the animation draw function, creates the visuals on the canvas
+    const draw = () => {
+      pulseCount.current++
+      let maxPrice = 0
+      let minPrice = 0
+
+      // find the highest price, and lowest price in the current queue
+      dataPoints.forEach(({price}) => {
+        if (price > maxPrice) {
+          maxPrice = price
+        } else if (price < minPrice || minPrice === 0) {
+          minPrice = price
+        }
+      })
+
+      // generic calculators to get the X and Y positions for each dataPoints point
+      const pulseRadius = 7 * Math.sin(pulseCount.current * 0.05) ** 2
+      const getX = (index) => (canvas.width / maxPoints) * index
+      const getY = (price) =>
+        Math.abs((canvas.height / (maxPrice - minPrice)) * (price - minPrice) - canvas.height)
+
+      // draw the pulse dot
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+      context.beginPath()
+      context.arc(
+        dataPoints.length >= canvas.width
+          ? getX(canvas.width - pulseRadius)
+          : dataPoints.length
+          ? getX(dataPoints.length - 1)
+          : getX(0),
+        dataPoints.length ? getY(dataPoints[dataPoints.length - 1].price) : getY(0),
+        pulseRadius,
+        0,
+        2 * Math.PI,
+      )
+      context.fill()
+
+      // draw the graph lines
+      context.beginPath()
+      dataPoints.forEach(({price}, i) => {
+        if (i === 0) {
+          context.moveTo(getX(i), getY(price))
+        } else {
+          context.lineTo(getX(i), getY(price))
+        }
+      })
+      context.stroke()
+
+      animationFrameId.current = window.requestAnimationFrame(draw)
+    }
+
+    draw()
+    return () => {
+      window.cancelAnimationFrame(animationFrameId.current)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataPoints, maxPoints])
+
+  return <canvas ref={canvasRef} />
+}
+
+export default PulseCanvas
