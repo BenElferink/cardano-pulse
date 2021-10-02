696959f4fcfa023370c9a86779ebc993c0cb906c
Ben Elferink <ben.elferink@icloud.com>
2021-10-02 14:16:44 +0300

    Fix: parse error

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 
 export default function useLocalStorage(key = '', defaultValue = null) {
-  const [value, setValue] = useState(JSON.parse(localStorage.getItem(key) || defaultValue))
+  const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) || defaultValue)
 
   useEffect(() => {
     if (value != null) localStorage.setItem(key, JSON.stringify(value))
